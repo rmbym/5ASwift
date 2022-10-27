1d9867e6751e96ba17953ef2a949c116c3cb4e66
Jovan <bienvenujovan@gmail.com>
2022-10-27 16:06:58 +0200

    Add JTJController & link to storyboard

---
+++ SKETUVEUX/JTJController.swift
@@ -0,0 +1,19 @@
+//
+//  JTJController.swift
+//  SKETUVEUX
+//
+//  Created by jtj on 27/10/2022.
+//
+
+import UIKit
+
+class JTJController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
