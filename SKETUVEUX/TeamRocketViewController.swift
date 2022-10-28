79e6f4d7afd809a4b81fc826b5e7defdb12f4caa
julienmarion <jmarion@getmeelo.com>
2022-10-28 10:37:24 +0200

    first commit from the team rocket

---
+++ SKETUVEUX/TeamRocketViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TeamRocketViewController.swift
+//  SKETUVEUX
+//
+//  Created by Julien Marion on 28/10/2022.
+//
+
+import Foundation
+import UIKit
+
+class TeamRocketViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
